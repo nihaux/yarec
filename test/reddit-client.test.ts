0d2cb77dd83ef71ea1b9de4c12a5aa9d36a19c7e
Nihau <nhauseux@prismamedia.com>
2019-03-06 11:39:02 +0100

    init

---
+++ test/reddit-client.test.ts
@@ -0,0 +1,14 @@
+import DummyClass from "../src/reddit-client"
+
+/**
+ * Dummy test
+ */
+describe("Dummy test", () => {
+  it("works if true is truthy", () => {
+    expect(true).toBeTruthy()
+  })
+
+  it("DummyClass is instantiable", () => {
+    expect(new DummyClass()).toBeInstanceOf(DummyClass)
+  })
+})
