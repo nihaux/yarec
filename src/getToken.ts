6461822cf7864056aba50f701c9c7a9a034d8b03
Nihau <nhauseux@prismamedia.com>
2019-03-10 23:07:05 +0100

    appOnly auth

--- src/getToken.ts
+++ src/getToken.ts
@@ -5,12 +5,15 @@ import { getBasicAuthHeader } from './utils/getBasicAuthHeader';
 import { validateTokenResponse } from './utils/validateTokenResponse';
 import { parseTokenResponse } from './utils/parseTokenResponse';
 
-export type GetAccessTokenArgs = {
-  readonly code: string;
+export type CommonGetAccessTokenArgs = {
   readonly redirectUri: string;
   readonly clientId: string;
   readonly clientSecret?: string;
 };
+
+export type WithCodeGetAccessTokenArgs = { readonly code: string } & CommonGetAccessTokenArgs;
+export type AppOnlyGetAccessTokenArgs = CommonGetAccessTokenArgs;
+export type GetAccessTokenArgs = WithCodeGetAccessTokenArgs | AppOnlyGetAccessTokenArgs;
 
 export type GetAccessTokenResponse = {
   readonly accessToken: string;
@@ -20,21 +23,28 @@ export type GetAccessTokenResponse = {
   readonly refreshToken?: string;
 };
 
-export const getToken = async ({
-  code,
-  redirectUri,
-  clientId,
-  clientSecret,
-}: GetAccessTokenArgs): Promise<GetAccessTokenResponse> => {
+const isWithCode = (params: GetAccessTokenArgs): params is WithCodeGetAccessTokenArgs => {
+  return (params as any).code !== undefined;
+};
+
+export const getToken = async (params: GetAccessTokenArgs): Promise<GetAccessTokenResponse> => {
+  const { clientId, clientSecret, redirectUri } = params;
   const url = 'https://www.reddit.com/api/v1/access_token';
   const extraHeaders = getBasicAuthHeader({ clientId, clientSecret });
-  const body = {
-    grant_type: 'authorization_code',
-    code,
-    redirect_uri: redirectUri,
-  };
 
-  const response = await makePost({ url, body, extraHeaders });
+  const response = await makePost({
+    url,
+    body: isWithCode(params)
+      ? { grant_type: 'authorization_code', code: params.code, redirect_uri: redirectUri }
+      : {
+          grant_type: clientSecret
+            ? 'client_credentials'
+            : 'https://oauth.reddit.com/grants/installed_client',
+          device_id: 'DO_NOT_TRACK_THIS_DEVICE',
+          redirect_uri: redirectUri,
+        },
+    extraHeaders,
+  });
 
   const jsonResponse = await response.json();
 
