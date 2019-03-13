62db1c27b280b839c76a398ed03b877c2144094d
Nihau <nhauseux@prismamedia.com>
2019-03-13 19:07:39 +0100

    fix bundle exporting buffer. fix bundling test.

--- rollup.config.ts
+++ rollup.config.ts
@@ -26,7 +26,9 @@ export default {
     // Compile TypeScript files
     typescript({ useTsconfigDeclarationDir: true }),
     // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
-    commonjs(),
+    commonjs({
+      include: 'node_modules/js-base64/**', // Default: undefined
+    }),
     // Allow node_modules resolution, so you can use 'external' to control
     // which external modules to include in the bundle
     // https://github.com/rollup/rollup-plugin-node-resolve#usage
